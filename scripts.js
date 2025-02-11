805e973198cd4cd9c10a6b226c09e8930d121625
Murilo <murkuehne@gmail.com>
2025-02-11 15:57:19 -0300

    Setting the input to format the value to BRL

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 // variaveis de DOM
 const amount = document.getElementById("amount");
 
+// captura o evento do input amount.
 amount.oninput = () => {
+  // substitui o valor de letras por ' '.
   let value = amount.value.replace(/\D/g, "");
 
-  amount.value = value;
+  // transforma o valor em centavos
+  value = Number(value) / 100;
+
+  //   atualiza o valor do input
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // formata o valor no padrao BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
